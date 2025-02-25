fd4db36746f7376b577debf3d185e81a75c7eda3
Stu Combs <stu.combs@icloud.com>
2025-02-25 17:56:34 -0600

    Initial Commit

---
+++ clippoTests/clippoTests.swift
@@ -0,0 +1,17 @@
+//
+//  clippoTests.swift
+//  clippoTests
+//
+//  Created by Stu Combs on 2/25/25.
+//
+
+import Testing
+@testable import clippo
+
+struct clippoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
